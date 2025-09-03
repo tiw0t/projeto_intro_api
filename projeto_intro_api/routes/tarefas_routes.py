96f76d9ab41d394ac7daf584aa64f7a8389ff1f7
Timóteo da Silva Bastos <timoteodasilvabastos15@gmail.com>
2025-09-03 07:26:32 -0300

    alterações 1.0

--- projeto_intro_api/routes/tarefas_routes.py
+++ projeto_intro_api/routes/tarefas_routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify, request
-from controllers.tarefa_controller import TarefasController
+from controllers.tarefa_controller import TarefaController
 
 tarefas_dp = Blueprint('tarefas', __name__)
 
 @tarefas_dp.route('/tarefas', methods=['GET'])
 def get_listar_tarefas():
-    return jsonify([tarefa.to_dict() for tarefa in TarefasController.get_listar_tarefa()])
+    return jsonify([tarefa.to_dict() for tarefa in TarefaController.get_listar_tarefas()])
